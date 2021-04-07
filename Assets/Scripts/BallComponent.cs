0fd8c8c8e6eaf6f84f9eff2f7d076271845867a9
Diversis95 <60331572+Diversis95@users.noreply.github.com>
2021-04-07 20:02:14 +0200

    Adding framerate counter

--- Assets/Scripts/BallComponent.cs
+++ Assets/Scripts/BallComponent.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class BallComponent : MonoBehaviour
 {
-    private int frame;
-    private float timer;
+    private int frame; //variable that shows counted frames
+    private float timer; //variable that count seconds
 
     // Start is called before the first frame update
     void Start()
     {
-        frame = 0;
-        timer = 0;
+        frame = 0; //setting frames to zero
+        timer = 0; //setting timer to zero
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        frame++;
-        if(timer > 1)
+        timer += Time.deltaTime; //timer counting every second
+        frame++; //adding every frame
+        if(timer > 1) //if timer exceed one second
         {
-            Debug.Log("Frames per seconds: " + frame);
-            frame = 0;
-            timer = 0;
+            Debug.Log("Frames per seconds: " + frame); //shows FPS
+            frame = 0; //setting frames to zero again
+            timer = 0; //setting timer to zero again
         }
         
     }
