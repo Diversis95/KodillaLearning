6b014f13b6760cb97828cfcf5244c0f1b7406b90
Diversis95 <60331572+Diversis95@users.noreply.github.com>
2021-04-07 19:23:29 +0200

    Old Project

---
+++ Assets/Scripts/BallComponent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallComponent : MonoBehaviour
+{
+    private int frame;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        frame = 0;
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        frame++;
+        if(timer > 1)
+        {
+            Debug.Log("Frames per seconds: " + frame);
+            frame = 0;
+            timer = 0;
+        }
+        
+    }
+}
