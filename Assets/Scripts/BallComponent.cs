eccab7f1eb72474318256c3f5519684fbe195aff
Diversis95 <60331572+Diversis95@users.noreply.github.com>
2021-04-08 08:57:43 +0200

    Scale exercise + extra FPS exercise

--- Assets/Scripts/BallComponent.cs
+++ Assets/Scripts/BallComponent.cs
@@ -1,30 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class BallComponent : MonoBehaviour
 {
-    private int frame; //variable that shows counted frames
-    private float timer; //variable that count seconds
+    private int frame;
+    private float timer;
+
+    private int[] numberOfFrame = new int[10];
+    private int thisFrame;
+    private int avarageFramerate;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        frame = 0; //setting frames to zero
-        timer = 0; //setting timer to zero
+        frame = 0;
+        timer = 0;
+        thisFrame = 0;
+        avarageFramerate = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime; //timer counting every second
-        frame++; //adding every frame
-        if(timer > 1) //if timer exceed one second
-        {
-            Debug.Log("Frames per seconds: " + frame); //shows FPS
-            frame = 0; //setting frames to zero again
-            timer = 0; //setting timer to zero again
-        }
+        timer += Time.deltaTime;
+        frame++;
+
+        CheckIfSecondPass();
+
         
+    }
+
+    void CheckIfSecondPass()
+    {
+        if (timer > 1)
+        {
+            Debug.Log("Frames per seconds: " + frame);
+
+            numberOfFrame[thisFrame] = frame;
+            thisFrame++;
+
+            if (thisFrame == 10)
+            {
+                AvarageCounter();
+                Debug.Log("Avarage 10 sec framerate: " + avarageFramerate);
+
+                thisFrame = 0;
+            }
+
+            frame = 0;
+            timer = 0;
+        }
+    }
+
+    void AvarageCounter()
+    {
+        for (int x = 0; x < numberOfFrame.Length; x++)
+        {
+            avarageFramerate += numberOfFrame[x];
+        }
+        avarageFramerate /= numberOfFrame.Length;
     }
 }
---
+++ Assets/Scripts/BallComponentTransform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallComponentTransform : MonoBehaviour
+{
+    public float transformingSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = Vector3.one;
+
+        transformingSpeed = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.localScale.x < 3.0f && transform.localScale.y < 3.0f && transform.localScale.z < 3.0f)
+        {
+            transform.localScale += Vector3.right * Time.deltaTime * transformingSpeed;
+            transform.localScale += Vector3.up * Time.deltaTime * transformingSpeed;
+            transform.localScale += Vector3.forward * Time.deltaTime * transformingSpeed;
+        }
+    }
+}
