0c79fdf017a00de66d0881c7917cf7d201844c10
Diversis95 <60331572+Diversis95@users.noreply.github.com>
2021-04-20 09:29:57 +0200

    Fixes for Exercise 2.2

---
+++ Assets/Scripts/BallComponents.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum BallInstruction
+{
+    Idle = 0,
+    MoveUp,
+    MoveDown,
+    MoveLeft,
+    MoveRight
+}
+
+public class BallComponents : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public List<BallInstruction> instructions = new List<BallInstruction>();
+    public int currentInstructionIndex = 0;
+    public float distance = 1.0f;
+
+    public Vector3 currentPosition;
+    public Vector3 startPosition;
+
+    public void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    public void Update()
+    {
+        if (currentInstructionIndex >= instructions.Count)
+            return;
+
+        float realSpeed = speed * Time.deltaTime;
+        float insctructionLenght = Vector3.Distance(startPosition, currentPosition);
+
+        switch (instructions[currentInstructionIndex])
+        {
+            case BallInstruction.MoveUp:
+                transform.position += Vector3.up * realSpeed;
+                currentPosition = transform.position;
+                break;
+
+            case BallInstruction.MoveDown:
+                transform.position += Vector3.down * realSpeed;
+                currentPosition = transform.position;
+                break;
+
+            case BallInstruction.MoveLeft:
+                transform.position += Vector3.left * realSpeed;
+                currentPosition = transform.position;
+                break;
+
+            case BallInstruction.MoveRight:
+                transform.position += Vector3.right * realSpeed;
+                currentPosition = transform.position;
+                break;
+
+            default:
+                Debug.Log("Idle");
+                break;
+        }
+
+        if (insctructionLenght > distance)
+        {
+            startPosition = currentPosition;
+            ++currentInstructionIndex;
+        }
+    }
+}
---
+++ Assets/Scripts/InputLesson.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputLesson : MonoBehaviour
+{
+    Rigidbody2D m_rigidbody;
+
+    private void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMouseUp()
+    {
+        m_rigidbody.simulated = true;
+    }
+
+    private void OnMouseDrag()
+    {
+        m_rigidbody.simulated = false;
+
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(worldPos.x, worldPos.y, 0);
+    }
+}
